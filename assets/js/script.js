5c266db7441eb8f7772c7bb0abbbf463367cc92b
caleb1711 <>
2023-10-03 20:09:11 +0000

    Add basic html structure for game div

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const myPlayer = document.querySelector("#myPlayer");
+const myComputer = document.querySelector("#myComputer");
+const myResult = document.querySelector("#myResult");
+const myButton = document.querySelectorAll(".myButton");
+
