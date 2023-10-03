25e46005e3ea97e556789aa7a4b7f97a6e9b5792
caleb1711 <>
2023-10-03 20:31:08 +0000

    iterate through the buttons, set player, computer and result text content

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ const myComputer = document.querySelector("#myComputer");
 const myResult = document.querySelector("#myResult");
 const myButton = document.querySelectorAll(".myButtons");
 
+let player;
+let computer;
+let result;
+
+myButton.forEach(button => button.addEventListener("click", () => {
+    button.textContent = player;
+    computersOptions();
+    myPlayer.textContent = `Player: ${player}`;
+    myComputer.textContent = `Computer: ${computer}`;
+    myResult.textContent = checkResults();
+}))
+
