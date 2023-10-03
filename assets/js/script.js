dcbdf0980df642cc6d4496edf96613497fcdf267
caleb1711 <>
2023-10-03 21:13:35 +0000

    set function to check results

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,33 +8,46 @@ let computer;
 let result;
 
 myButton.forEach(button => button.addEventListener("click", () => {
-    button.textContent = player;
+    player = button.textContent;
     computersOptions();
     myPlayer.textContent = `Player: ${player}`;
     myComputer.textContent = `Computer: ${computer}`;
     myResult.textContent = checkResults();
-}))
+}));
 
-function computersOptions(){
+function computersOptions() {
     const selections = Math.floor(Math.random() * 3) + 1;
-    switch(selections){
+    switch (selections) {
         case 1:
             computer = "ROCK";
             break;
 
-            case 2:
-                computer = "PAPER";
-                break;
+        case 2:
+            computer = "PAPER";
+            break;
 
-                case 3:
-                    computer = "SCISSORS";
-                    break;
+        case 3:
+            computer = "SCISSORS";
+            break;
 
-                    default:
-                        break;
+        default:
+            break;
 
     }
 };
 
-func
+function checkResults() {
+    if (computer == player) {
+        return ("DRAW!");
+    }
+    if (computer == "ROCK") {
+        return (player == "PAPER") ? "You Win!" : "You Loose!";
+    }
+    if (computer == "SCISSORS") {
+        return (player == "ROCK") ? "You Win" : "You Loose!";
+    }
+    if (computer == "PAPER") {
+        return (player == "SCISSORS") ? "You Win" : "You Loose!";
+    }
+}
 
